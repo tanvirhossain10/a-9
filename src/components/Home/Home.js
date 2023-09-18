eaeb1af3b87dea0518a2562d1e7f3bf2bc95c9a0
tanvirhossain10 <tanvirhossen808@gmail.com>
2023-09-18 23:44:40 +0600

    add see all button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,11 +25,12 @@ const Home = () => {
             <div>
                 <h2 className='text-4xl text-cyan-600 mt-40 mb-40'>Customer Reviews:({homePageReviews.length})</h2>
                 <div className="flex justify-center  mb-36 ">
-                    {homePageReviews.map(review => <div key={review.id} className='bg-slate-400 p-5 mx-28 text-left'><h3 className=' text-3xl p-5'>Name:{review.name}</h3>
+                    {homePageReviews.map(review => <div key={review.id} className='bg-slate-400 p-5 mx-28 text-left drop-shadow-xl shadow-lg shadow-blue-500/50 '><h3 className=' text-3xl p-5'>Name:{review.name}</h3>
                         <p>{review.body}</p>
                         <p className='text-center mt-4 '><small className=' text-2xl bg-green-500 text-blue-700 rounded-full p-2  '>Star:{review.star}</small></p>
                     </div>)}
                 </div>
+                <button className='mb-10 bg-slate-600 text-slate-50 p-2 px-6 hover:bg-red-700 rounded-full'>See all Reviews</button>
             </div>
         </div>
 
