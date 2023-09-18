44cd50007d43db19a8e7cac0e5d43d3297fcddd5
tanvirhossain10 <tanvirhossen808@gmail.com>
2023-09-18 16:37:07 +0600

    Push the header components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='flex justify-center mt-10'>
+                <NavLink to='/home' className='mr-20 ml-20'>Home</NavLink>
+                <NavLink to='/reviews' className='mr-20 ml-20'>REVIEWS</NavLink>
+                <NavLink to='/dashboard' className='mr-20 ml-20'>DASHBOARD</NavLink>
+                <NavLink to='/blog' className='mr-20 ml-20'>BLOG</NavLink>
+                <NavLink to='/about' className='mr-20 ml-20'>ABOUT</NavLink>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
