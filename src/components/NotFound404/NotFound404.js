17b0627b878fcb6f97e656f159ede3b29d3c3cda
tanvirhossain10 <tanvirhossen808@gmail.com>
2023-09-19 16:22:46 +0600

    fix the bugs of 404 picture

--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -5,7 +5,7 @@ const NotFound404 = () => {
         <div className='h-screen bg-gradient-to-b
         from-green-200 to-green-500'>
             <div className='flex w-full  justify-evenly items-center pt-10'>
-                <img className="w-3/5" src="hamba.jpg" alt="" />
+                <img className="w-3/5" src="/hamba.jpg" alt="" />
 
                 <h2 className='text-4xl bg-gradient-to-r bg-clip-text  text-transparent 
             from-indigo-500 via-orange-500 to-red-700
